871820ba74da167ce8a654e4cb67b84f8a1985f7
DrunkenAlcoholic <drunkenalcoholic@gmail.com>
2024-05-19 00:15:08 +0800

    Update getPulse.nim
    Update getPulse

--- etc/skel/.config/nimdow/nimdowstatus/src/functions/getPulse.nim
+++ etc/skel/.config/nimdow/nimdowstatus/src/functions/getPulse.nim
@@ -1,6 +1,8 @@
 proc getPulse(): string = 
-  let 
-    volLevel = execProcess("pactl get-sink-volume @DEFAULT_SINK@").split(" / ")[1].strip
-    isMuted = execProcess("pactl get-sink-mute @DEFAULT_SINK@").contains("yes")
-    icon = if volLevel == "0%" or isMuted: MUTE_ICON else: VOL_ICON
+  let volLevel = execProcess("pactl get-sink-volume @DEFAULT_SINK@").split(" / ")[1].strip
+  let isMuted = execProcess("pactl get-sink-mute @DEFAULT_SINK@").contains("yes")
+  let icon = if volLevel == "0%" or isMuted:
+    MUTE_ICON
+  else:
+    VOL_ICON
   icon & " " & volLevel
